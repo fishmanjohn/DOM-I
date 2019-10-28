c0943a13c188f8cfd3ea0a60ab4fe8acda2ecd0c
fishmanjohn <john@emmaandjohnbyvan.com>
2019-10-28 15:55:21 -0700

    commiting changes

--- js/index.js
+++ js/index.js
@@ -40,3 +40,41 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let nav = document.getElementsByTagName('a');
+nav[0].textContent = siteContent.nav["nav-item-1"];
+nav[1].textContent = siteContent.nav["nav-item-2"];
+nav[2].textContent = siteContent.nav["nav-item-3"];
+nav[3].textContent = siteContent.nav["nav-item-4"];
+nav[4].textContent = siteContent.nav["nav-item-5"];
+nav[5].textContent = siteContent.nav["nav-item-6"];
+
+let head1 = document.querySelector('.cta-text h1');
+head1.textContent = siteContent.cta["h1"];
+
+let aweButt = document.getElementsByName('button');
+aweButt = siteContent["cta"]["button"];
+
+let codeCircle = document.getElementById("cta-img");
+codeCircle.setAttribute('src', siteContent["cta"]["img-src"])
+
+let featHead = document.querySelector('.text-content h4');
+featHead.textContent = siteContent["main-content"]["features-h4"];
+
+let featText = document.querySelector('.text-content p');
+featText.textContent = siteContent["main-content"]["features-content"];
+
+let aboutHead = document.querySelector('.top-content div:last-child h4');
+aboutHead.textContent = siteContent["main-content"]["about-h4"];
+
+let aboutText = document.querySelector('.top-content div:last-child p');
+aboutText.textContent = siteContent["main-content"]["about-content"];
+
+let codeBanner = document.getElementById('middle-img');
+codeBanner.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+let servicesHead = document.querySelector('.bottom-content :first-child h4 ');
+servicesHead.textContent = siteContent["main-content"]["services-h4"];
+
+let servicesText = document.querySelector('.bottom-content :first-child p');
+servicesText.textContent = siteContent["main-content"]["services-content"];
